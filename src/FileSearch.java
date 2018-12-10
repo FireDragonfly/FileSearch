42af04c33eece00b68f9fca817293fadc908d749
marieign <marieignn@gmail.com>
2018-12-10 21:28:34 +0200

    111

--- src/FileSearch.java
+++ src/FileSearch.java
@@ -1,41 +1,26 @@
-import java.io.File;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.io.InputStreamReader;
 
-public class  FileSearch {
-public static void main (String[]args) throws IOException {
-    Path path = Paths.get("src/gamlet.txt").toAbsolutePath();
-    List<String> titles = Files.lines(path).collect(Collectors.toList());
-
-    String searchGame = getInput();
-
-    displayResults(searchGame, titles);
-}
-
-public static String getInput() {
-        Scanner sc= new Scanner(System.in);
-        System.out.print("Write the words you need to find in the book: ");
-        String titlename =  sc.nextLine();
-        sc.close();
-
-        return titlename;
-}
-
-public static void displayResults(String searchName,List<String> titles) throws IOException {
-    boolean inFile= titles.stream().anyMatch(p->p.equalsIgnoreCase(searchName));
-
-    if (inFile) {
-        System.out.println("\nYes, "+searchName+" this word is IN this book!");
-    } else {
-        System.out.println("\nNo, "+searchName+" this word is NOT in this book!");
-
+public class FileSearch {
+    public static void main(String[] args) {
+        try {
+            BufferedInputStream in = new BufferedInputStream(new FileInputStream("src/gamlet.txt"));
+            byte arr[] = new byte[in.available()];
+            in.read(arr);
+            String text = new String(arr);
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String keyword;
+            keyword = br.readLine();
+            if(keyword.contains(" "))
+                keyword = keyword.substring(0,keyword.indexOf(' '));
+            if(text.contains(keyword)){
+                System.out.println(keyword);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
-
-}
-
