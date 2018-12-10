bde8d691787477fc4e7fed0edbce4d7bd6985dfe
marieign <marieignn@gmail.com>
2018-12-10 23:57:36 +0200

    Реализовано открытие файла и разбиение на строки в класе SearchEngine

--- src/SearchEngine.java
+++ src/SearchEngine.java
@@ -1,12 +1,21 @@
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class SearchEngine {
 
-    public static SearchResult find(String query, Path path) {
+    public static SearchResult find(String query, String filePath) {
 
-
+        try {
+            List<String> fileLines = Files.readAllLines(Paths.get(filePath), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return new SearchResult( 0);
      }
